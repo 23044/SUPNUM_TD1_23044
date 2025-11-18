1c6a9cbee38df98bef4c9c00f795e99280cd5879
Aichetou Cheibeta <23044@supnum.mr>
2025-11-18 09:11:56 +0000

    q5: implementation de l'entity service logique et repository

--- src/main/java/com/example/SUPNUM_TD1_23044/model/Server.java
+++ src/main/java/com/example/SUPNUM_TD1_23044/model/Server.java
@@ -1,5 +1,58 @@
 package com.example.SUPNUM_TD1_23044.model;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Server {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String ipAddress;
+
+    private Boolean status = false;
+
+    public Server() {}
+
+    public Server(String name, String ipAddress) {
+        this.name = name;
+        this.ipAddress = ipAddress;
+        this.status = false;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    public void setIpAddress(String ipAddress) {
+        this.ipAddress = ipAddress;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
 }
--- src/main/java/com/example/SUPNUM_TD1_23044/repository/ServerRepository.java
+++ src/main/java/com/example/SUPNUM_TD1_23044/repository/ServerRepository.java
@@ -1,5 +1,11 @@
 package com.example.SUPNUM_TD1_23044.repository;
 
-public class ServerRepository {
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.SUPNUM_TD1_23044.model.Server;
+
+@Repository
+public interface ServerRepository extends JpaRepository<Server, Long> {
+
 }
